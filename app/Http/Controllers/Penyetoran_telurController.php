2f42f193b104d8bad11adeaf56e7eb0828da832b
nadws <nandw567@gmail.com>
2023-08-08 16:08:18 +0800

    dsa

--- app/Http/Controllers/Penyetoran_telurController.php
+++ app/Http/Controllers/Penyetoran_telurController.php
@@ -52,7 +52,7 @@ class Penyetoran_telurController extends Controller
             left join akun as b on b.id_akun = a.id_akun
             where a.id_buku = '6' and a.id_akun IN('64','25','82') and a.setor ='T' and a.debit != '0'
             group by a.no_nota
-            order by a.tgl ASC
+            order by a.tgl , a.no_nota ASC
             ")
         ];
         return view('penyetoran.index', $data);
