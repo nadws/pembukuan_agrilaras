4e25f23ec244b4e6f5d99d2d21ae3149c330a70e
nadws <nandw567@gmail.com>
2023-09-16 11:33:22 +0800

    das

--- resources/views/penjualan_agl/export.blade.php
+++ resources/views/penjualan_agl/export.blade.php
@@ -41,7 +41,8 @@
             <td>{{ number_format($i->total_rp,0) }}</td>
             <td>{{ $i->tipe }}</td>
             <td>{{$i->kredit - $i->debit == 0 ? 'Paid' : 'Unpaid'}}</td>
-            <td>{{empty($i->tgl_setor) ? '-' : tanggal($i->tgl_setor)}}</td>
+            <td>{{empty($i->tgl_setor) ? '-' :(empty($i->akun_setor) ? tanggal($i->tgl_stor_kosong) :
+                tanggal($i->tgl_setor))}}</td>
             <td>{{empty($i->nota_setor) ? '-' : $i->nota_setor}}</td>
             <td>{{empty($i->nota_setor) ? '-' : (empty($i->akun_setor) ? 'BCA' : $i->akun_setor)}}</td>
             <td>{{$i->admin}}</td>
