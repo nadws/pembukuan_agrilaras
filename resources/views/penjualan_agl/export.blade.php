0b5e49f2f87702d41f5074ba4f57215a4e588adb
nadws <nandw567@gmail.com>
2023-09-16 11:26:08 +0800

    dsa

--- resources/views/penjualan_agl/export.blade.php
+++ resources/views/penjualan_agl/export.blade.php
@@ -43,7 +43,7 @@
             <td>{{$i->kredit - $i->debit == 0 ? 'Paid' : 'Unpaid'}}</td>
             <td>{{empty($i->tgl_setor) ? '-' : tanggal($i->tgl_setor)}}</td>
             <td>{{empty($i->nota_setor) ? '-' : $i->nota_setor}}</td>
-            <td>{{empty($i->nota_setor) ? '-' : $i->akun_setor}}</td>
+            <td>{{empty($i->nota_setor) ? '-' : (empty($i->akun_setor) ? 'BCA' : $i->akun_setor)}}</td>
             <td>{{$i->admin}}</td>
         </tr>
         @endforeach
