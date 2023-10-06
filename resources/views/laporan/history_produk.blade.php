7763341b029da311ca802d7f2334bdd431801a50
nadws <nandw567@gmail.com>
2023-10-06 13:05:55 +0800

    dsadsa

--- resources/views/laporan/history_produk.blade.php
+++ resources/views/laporan/history_produk.blade.php
@@ -1,5 +1,8 @@
 <div class="row">
     <div class="col-lg-12 text-center">
+        <h5>{{ $history->nm_produk }}</h5>
+    </div>
+    <div class="col-lg-12 text-center">
         <p>{{ $history->kegunaan }}</p>
     </div>
 </div>
