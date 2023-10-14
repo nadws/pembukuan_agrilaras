9a06ace4b8ada0c9448e6d12951f453d9e77727b
nadws <nandw567@gmail.com>
2023-10-14 10:24:47 +0800

    dsada

--- resources/views/penjualan_agl/export.blade.php
+++ resources/views/penjualan_agl/export.blade.php
@@ -21,32 +21,36 @@
     </thead>
     <tbody>
         @foreach ($invoice as $no => $i)
-        <tr>
-            <td>{{ $no + 1 }}</td>
-            <td>{{ tanggal($i->tgl) }}</td>
-            <td>{{ $i->no_nota }}</td>
-            <td>
-                @if ($i->lokasi == 'alpa')
-                {{ $i->nm_customer }}{{$i->urutan_customer}}
-                @else
-                {{ $i->customer }}
-                @endif
+            <tr>
+                <td>{{ $no + 1 }}</td>
+                <td>{{ tanggal($i->tgl) }}</td>
+                <td>{{ $i->no_nota }}</td>
+                <td>
+                    @if ($i->lokasi == 'alpa')
+                        {{ $i->nm_customer }}{{ $i->urutan_customer }}
+                    @else
+                        {{ $i->customer }}
+                    @endif
 
-            </td>
-            <td>{{$i->driver}}</td>
-            <td>{{$i->pcs}}</td>
-            <td>{{number_format($i->kg,1)}}</td>
-            <td>{{number_format($i->pcs / 180,1)}}</td>
-            <td>{{number_format($i->kg_jual,1)}}</td>
-            <td>{{ number_format($i->total_rp,0) }}</td>
-            <td>{{ $i->tipe }}</td>
-            <td>{{$i->kredit - $i->debit == 0 ? 'Paid' : 'Unpaid'}}</td>
-            <td>{{empty($i->nota_setor) ? '-' : (empty($i->akun_setor) ? tanggal($i->tgl_stor_kosong) :
-                tanggal($i->tgl_setor))}}</td>
-            <td>{{empty($i->nota_setor) ? '-' : $i->nota_setor}}</td>
-            <td>{{empty($i->nota_setor) ? '-' : (empty($i->akun_setor) ? 'BCA' : $i->akun_setor)}}</td>
-            <td>{{$i->admin}}</td>
-        </tr>
+                </td>
+                <td>{{ $i->driver }}</td>
+                <td>{{ $i->pcs }}</td>
+                <td>{{ number_format($i->kg, 1) }}</td>
+                <td>{{ number_format($i->pcs / 180, 1) }}</td>
+                <td>{{ number_format($i->kg_jual, 1) }}</td>
+                <td>{{ number_format($i->total_rp, 0) }}</td>
+                <td>{{ $i->lokasi == 'mtd' ? '' : $i->tipe }}</td>
+                <td>{{ $i->kredit - $i->debit == 0 ? 'Paid' : 'Unpaid' }}</td>
+                <td>{{ empty($i->nota_setor)
+                    ? '-'
+                    : (empty($i->akun_setor)
+                        ? tanggal($i->tgl_stor_kosong)
+                        : tanggal($i->tgl_setor)) }}
+                </td>
+                <td>{{ empty($i->nota_setor) ? '-' : $i->nota_setor }}</td>
+                <td>{{ empty($i->nota_setor) ? '-' : (empty($i->akun_setor) ? 'BCA' : $i->akun_setor) }}</td>
+                <td>{{ $i->admin }}</td>
+            </tr>
         @endforeach
     </tbody>
 </table>
