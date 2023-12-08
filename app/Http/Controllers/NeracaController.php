8510b60553dbcc7c7b87e059e71289f317b1162a
aldmf26 <aldimf26@gmail.com>
2023-12-08 09:08:15 +0800

    321

--- app/Http/Controllers/NeracaController.php
+++ app/Http/Controllers/NeracaController.php
@@ -13,12 +13,187 @@ class NeracaController extends Controller
     {
         $bulan =  $r->bulan ?? date('m');
         $tahun =  $r->tahun ?? date('Y');
+        $tgl1 =  '2020-01-01';
+        $tgl2 = date("$tahun-$bulan-t");
+
+        $kas  = NeracaModel::GetKas($tgl1, $tgl2, 1);
+        $bank  = NeracaModel::GetKas($tgl1, $tgl2, 2);
+        $piutang  = NeracaModel::GetKas($tgl1, $tgl2, 7);
+        $persediaan  = NeracaModel::GetKas($tgl1, $tgl2, 6);
+
         $data = [
             'title' => 'Laporan Neraca',
             'bulan' => $bulan,
             'bulans' => DB::table('bulan')->get(),
             'tahun' => $tahun,
+            'kas' => $kas,
+            'bank' => $bank,
+            'piutang' => $piutang,
+            'persediaan' => $persediaan,
+            'tgl1' => $tgl1,
+            'tgl2' => $tgl2,
         ];
         return view('neraca.index', $data);
+    }
+    public function loadneraca(Request $r)
+    {
+        $tgl1 =  '2020-01-01';
+        $tgl2 = $r->tgl2;
+
+        $tgl_1 = date('Y-m-01', strtotime($r->tgl2));;
+
+        $kas  = NeracaModel::GetKas($tgl1, $tgl2, 1);
+        $bank  = NeracaModel::GetKas($tgl1, $tgl2, 2);
+        $piutang  = NeracaModel::GetKas($tgl1, $tgl2, 7);
+        $persediaan  = NeracaModel::GetKas($tgl1, $tgl2, 6);
+        $hutang  = NeracaModel::GetKas($tgl1, $tgl2, 9);
+        $ekuitas  = NeracaModel::GetKas2($tgl1, $tgl2);
+        $ekuitas2  = NeracaModel::GetKas3($tgl1, $tgl2);
+        $laba_berjalan = NeracaModel::laba_berjalan_pendapatan($tgl1, $tgl2);
+        $laba_berjalan2 = NeracaModel::laba_berjalan_biaya($tgl1, $tgl2);
+
+        $peralatan  = NeracaModel::Getakumulasi($tgl1, $tgl2, 16);
+        $aktiva  = NeracaModel::Getakumulasi($tgl1, $tgl2, 9);
+        $aktiva_gantung  = NeracaModel::Getakumulasi($tgl1, $tgl2, 43);
+        $peralatan_gantung  = NeracaModel::Getakumulasi($tgl1, $tgl2, 61);
+        $pullet_gantung  = NeracaModel::Getakumulasi($tgl1, $tgl2, 76);
+
+        $akumulasi_aktiva  = NeracaModel::Getakumulasi($tgl1, $tgl2, 52);
+        $akumulasi_peralatan  = NeracaModel::Getakumulasi($tgl1, $tgl2, 59);
+
+        $data = [
+            'kas' => $kas,
+            'bank' => $bank,
+            'piutang' => $piutang,
+            'persediaan' => $persediaan,
+            'peralatan' => $peralatan,
+            'peralatan_gantung' => $peralatan_gantung,
+            'pullet_gantung' => $pullet_gantung,
+            'akumulasi' => $akumulasi_aktiva,
+            'akumulasi_peralatan' => $akumulasi_peralatan,
+            'aktiva' => $aktiva,
+            'aktiva_gantung' => $aktiva_gantung,
+            'hutang' => $hutang,
+            'ekuitas' => $ekuitas,
+            'ekuitas2' => $ekuitas2,
+            'tgl1' => $tgl1,
+            'tgl2' => $tgl2,
+            'laba_pendapatan' => $laba_berjalan,
+            'laba_biaya' => $laba_berjalan2,
+
+        ];
+        return view('neraca.load', $data);
+    }
+
+    function print_neraca(Request $r)
+    {
+        $tgl1 =  '2020-01-01';
+        $tgl2 = $r->tgl2;
+
+        $tgl_1 = date('Y-m-01', strtotime($r->tgl2));
+
+        $kas  = NeracaModel::GetKas($tgl1, $tgl2, 1);
+        $bank  = NeracaModel::GetKas($tgl1, $tgl2, 2);
+        $piutang  = NeracaModel::GetKas($tgl1, $tgl2, 7);
+        $hutang  = NeracaModel::GetKas($tgl1, $tgl2, 9);
+        $persediaan  = NeracaModel::GetKas($tgl1, $tgl2, 6);
+
+        $ekuitas  = NeracaModel::GetKas2($tgl1, $tgl2);
+        $ekuitas2  = NeracaModel::GetKas3($tgl1, $tgl2);
+        $laba_berjalan = NeracaModel::laba_berjalan_pendapatan($tgl1, $tgl2);
+        $laba_berjalan2 = NeracaModel::laba_berjalan_biaya($tgl1, $tgl2);
+
+        $peralatan  = NeracaModel::Getakumulasi($tgl1, $tgl2, 16);
+        $aktiva  = NeracaModel::Getakumulasi($tgl1, $tgl2, 9);
+        $aktiva_gantung  = NeracaModel::Getakumulasi($tgl1, $tgl2, 43);
+        $peralatan_gantung  = NeracaModel::Getakumulasi($tgl1, $tgl2, 61);
+        $pullet_gantung  = NeracaModel::Getakumulasi($tgl1, $tgl2, 76);
+
+        $akumulasi_aktiva  = NeracaModel::Getakumulasi($tgl1, $tgl2, 52);
+        $akumulasi_peralatan  = NeracaModel::Getakumulasi($tgl1, $tgl2, 59);
+
+        $data = [
+            'kas' => $kas,
+            'bank' => $bank,
+            'piutang' => $piutang,
+            'peralatan' => $peralatan,
+            'peralatan_gantung' => $peralatan_gantung,
+            'pullet_gantung' => $pullet_gantung,
+            'akumulasi' => $akumulasi_aktiva,
+            'akumulasi_peralatan' => $akumulasi_peralatan,
+            'aktiva' => $aktiva,
+            'aktiva_gantung' => $aktiva_gantung,
+            'hutang' => $hutang,
+            'ekuitas' => $ekuitas,
+            'ekuitas2' => $ekuitas2,
+            'persediaan' => $persediaan,
+            'tgl1' => $tgl1,
+            'tgl2' => $tgl2,
+            'laba_pendapatan' => $laba_berjalan,
+            'laba_biaya' => $laba_berjalan2,
+
+        ];
+        return view('neraca.print', $data);
+    }
+
+    public function loadinputSub_neraca(Request $r)
+    {
+        $data = [
+            'subkategori' => DB::table('sub_kategori_neraca')->where('id_kategori', $r->kategori)->get(),
+            'kategori' => $r->kategori
+        ];
+        return view('neraca.inputSub', $data);
+    }
+
+    public function saveSub_neraca(Request $r)
+    {
+        $data = [
+            'nama_sub_kategori' => $r->nama_sub_kategori,
+            'id_kategori' => $r->kategori,
+            'urutan' => $r->urutan
+        ];
+        DB::table('sub_kategori_neraca')->insert($data);
+    }
+
+    public function loadinputAkun_neraca(Request $r)
+    {
+        $data = [
+            'akun_neraca' => DB::select("SELECT a.id_akun_neraca, a.id_akun, b.nm_akun, c.debit , c.kredit
+            FROM akun_neraca as a
+            left join akun as b on b.id_akun = a.id_akun
+            left join (
+            SELECT c.id_akun, sum(c.debit) as debit, sum(c.kredit) as kredit
+                FROM jurnal as c
+                where c.tgl BETWEEN '2023-01-01' and '$r->tgl2'
+                group by c.id_akun
+            ) as c on c.id_akun = a.id_akun
+            WHERE a.id_sub_kategori = '$r->id_sub_kategori';"),
+            'id_sub_kategori' => $r->id_sub_kategori,
+            'akun' => DB::select("SELECT * FROM akun as a where a.id_akun not in(SELECT b.id_akun FROM akun_neraca as b)")
+        ];
+        return view('neraca.inputAkun', $data);
+    }
+
+    public function saveAkunNeraca(Request $r)
+    {
+        $data = [
+            'id_akun' => $r->id_akun,
+            'id_sub_kategori' => $r->id_sub_kategori,
+        ];
+        DB::table('akun_neraca')->insert($data);
+    }
+
+    public function delete_akun_neraca(Request $r)
+    {
+        DB::table('akun_neraca')->where('id_akun_neraca', $r->id_akun_neraca)->delete();
+    }
+    public function view_akun_neraca()
+    {
+        $data = [
+            'akun' => DB::Select("SELECT a.id_akun,a.nm_akun, b.id_akun as ada FROM akun as a
+            LEFT JOIN akun_neraca as b ON a.id_akun= b.id_akun"),
+        ];
+
+        return view('neraca.view_akun', $data);
     }
 }
--- resources/views/neraca/index.blade.php
+++ resources/views/neraca/index.blade.php
@@ -1,4 +1,4 @@
-<x-theme.app title="{{ $title }}" table="Y" sizeCard="8" cont="container-fluid">
+<x-theme.app title="{{ $title }}" table="Y" sizeCard="7" cont="container-fluid">
     <x-slot name="cardHeader">
         <style>
             .freeze-cell1_th {
@@ -68,11 +68,114 @@
         </div>
     </x-slot>
     <x-slot name="cardBody">
+
         <div class="row">
-            <table class="table table-bordered">
+            <table class="table table-bordered table-hover table-striped" x-data="{
+                open1: false,
+                open2: false,
+                open3: false,
+                open4: false,
+            }">
                 <thead>
                     <tr>
-                        <th class="dhead"></th>
+                        <th class="dhead">Aktiva</th>
+                    </tr>
+                    <tr>
+                        <th class="ps-4">Aktiva Lancar</th>
+                    </tr>
+                    @php
+                        $ttl_kas = 0;
+                        $ttl_bank = 0;
+                        $ttl_piutang = 0;
+                        $ttl_persediaan = 0;
+
+                        foreach ($kas as $k) {
+                            $ttl_kas += $k->debit + $k->debit_saldo - ($k->kredit + $k->kredit_saldo);
+                        }
+                        foreach ($bank as $k) {
+                            $ttl_bank += $k->debit + $k->debit_saldo - ($k->kredit + $k->kredit_saldo);
+                        }
+
+                        foreach ($piutang as $k) {
+                            $ttl_piutang += $k->debit + $k->debit_saldo - ($k->kredit + $k->kredit_saldo);
+                        }
+                        foreach ($persediaan as $k) {
+                            $ttl_persediaan += $k->debit + $k->debit_saldo - ($k->kredit + $k->kredit_saldo);
+                        }
+                    @endphp
+                    <tr>
+
+                        <td class="ps-5">
+                            <div style="cursor: pointer" class="float-start me-2" @click="open1 = ! open1"><i
+                                    class=" fas fa-caret-down"></i>
+                                KAS
+                            </div>
+                            <span class="float-end me-5">Rp {{ number_format($ttl_kas, 0) }}</span>
+
+                        </td>
+                    </tr>
+                    @foreach ($kas as $k)
+                        <tr  x-show="open1">
+                            <td class="ps-5">
+                                <a target="_blank"
+                                    href="{{ route('summary_buku_besar.detail', ['id_akun' => $k->id_akun, 'tgl1' => $tgl1, 'tgl2' => $tgl2]) }}">{{ $k->nm_akun }}
+                                </a>
+                                <span
+                                    class="float-end me-5">{{ number_format($k->debit + $k->debit_saldo - $k->kredit - $k->kredit_saldo, 0) }}</span>
+
+
+                            </td>
+
+                        </tr>
+                    @endforeach
+                    <tr>
+
+                        <td class="ps-5">
+                            <div style="cursor: pointer" class="float-start me-2" @click="open2 = ! open2"><i
+                                    class=" fas fa-caret-down"></i>
+                                BANK
+                            </div>
+                            <span class="float-end me-5">Rp {{ number_format($ttl_bank, 0) }}</span>
+
+                        </td>
+                    </tr>
+                    @foreach ($bank as $k)
+                        <tr  x-show="open2">
+                            <td class="ps-5">
+                                <a target="_blank"
+                                    href="{{ route('summary_buku_besar.detail', ['id_akun' => $k->id_akun, 'tgl1' => $tgl1, 'tgl2' => $tgl2]) }}">{{ $k->nm_akun }}
+                                </a>
+                                <span
+                                    class="float-end me-5">{{ number_format($k->debit + $k->debit_saldo - $k->kredit - $k->kredit_saldo, 0) }}</span>
+                            </td>
+
+                        </tr>
+                    @endforeach
+                    <tr>
+
+                        <td class="ps-5">
+                            <div style="cursor: pointer" class="float-start me-2" @click="open2 = ! open2"><i
+                                    class=" fas fa-caret-down"></i>
+                                PIUTANG DAGANG
+                            </div>
+                            <span class="float-end me-5">Rp {{ number_format($ttl_bank, 0) }}</span>
+
+                        </td>
+                    </tr>
+                    @foreach ($piutang as $k)
+                        <tr  x-show="open2">
+                            <td class="ps-5">
+                                <a target="_blank"
+                                    href="{{ route('summary_buku_besar.detail', ['id_akun' => $k->id_akun, 'tgl1' => $tgl1, 'tgl2' => $tgl2]) }}">{{ $k->nm_akun }}
+                                </a>
+                                <span
+                                    class="float-end me-5">{{ number_format($k->debit + $k->debit_saldo - $k->kredit - $k->kredit_saldo, 0) }}</span>
+                            </td>
+
+                        </tr>
+                    @endforeach
+                    <tr>
+                        <th class="ps-4">Total Aktiva Lancar</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -83,3 +186,4 @@
     </x-slot>
 
 </x-theme.app>
+
