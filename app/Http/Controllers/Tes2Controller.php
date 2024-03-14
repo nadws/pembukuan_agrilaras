1ca342a581dc7d479f1db37ada7c0670cbe09987
nadws <nandw567@gmail.com>
2024-03-14 14:56:10 +0800

    dsada

---
+++ app/Http/Controllers/Tes2Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Tes2Controller extends Controller
+{
+    function index(Request $r)
+    {
+        $NANDA = 'DSA';
+    }
+}
