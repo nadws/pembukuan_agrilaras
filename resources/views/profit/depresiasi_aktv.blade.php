e4b688ba3c30491618f065afbd3be60aa7863655
nadws <nandw567@gmail.com>
2025-01-06 10:55:22 +0800

    update depresiasi_aktv.blade.php and depresiasi_peralatan.blade.php

--- resources/views/profit/depresiasi_aktv.blade.php
+++ resources/views/profit/depresiasi_aktv.blade.php
@@ -4,27 +4,29 @@
         $ttl += $a->b_penyusutan;
     }
 @endphp
-<table class="table table-bordered table-responsive" id="table_detail_akt">
-    <thead>
-        <tr>
-            <th class="dhead">No</th>
-            <th class="dhead">Nama aktiva</th>
-            <th class="dhead">Tangal Perolehan</th>
-            <th class="dhead">Nama kelompok</th>
-            <th class="dhead text-end">Umur</th>
-            <th class="dhead text-end">Biaya depresiasi <br> {{ number_format($ttl, 0) }}</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($depaktiva as $no => $d)
+<div class="table-responsive">
+    <table class="table table-bordered table-responsive" id="table_detail_akt">
+        <thead>
             <tr>
-                <td>{{ $no + 1 }}</td>
-                <td>{{ $d->nm_aktiva }}</td>
-                <td>{{ tanggal($d->tgl_perolehan) }}</td>
-                <td>{{ $d->nm_kelompok }}</td>
-                <td class="text-end">{{ $d->umur }} tahun</td>
-                <td class="text-end">{{ number_format($d->b_penyusutan, 0) }}</td>
+                <th class="dhead">No</th>
+                <th class="dhead">Nama aktiva</th>
+                <th class="dhead">Tangal Perolehan</th>
+                <th class="dhead">Nama kelompok</th>
+                <th class="dhead text-end">Umur</th>
+                <th class="dhead text-end">Biaya depresiasi <br> {{ number_format($ttl, 0) }}</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($depaktiva as $no => $d)
+                <tr>
+                    <td>{{ $no + 1 }}</td>
+                    <td>{{ $d->nm_aktiva }}</td>
+                    <td>{{ tanggal($d->tgl_perolehan) }}</td>
+                    <td>{{ $d->nm_kelompok }}</td>
+                    <td class="text-end">{{ $d->umur }} tahun</td>
+                    <td class="text-end">{{ number_format($d->b_penyusutan, 0) }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
