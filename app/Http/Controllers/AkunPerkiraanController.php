e1f7a71d3e5b2cfa2bc6b22964213592cefdeec3
nadws <nandw567@gmail.com>
2025-11-27 11:54:05 +0800

    update AkunPerkiraanController.php

--- app/Http/Controllers/AkunPerkiraanController.php
+++ app/Http/Controllers/AkunPerkiraanController.php
@@ -6,6 +6,7 @@ use App\Models\LaporanLayerModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\IOFactory;
